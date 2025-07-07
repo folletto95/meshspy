b3cf7000f1efd681ff94702dea791623f9ee82f1
Niccolò Malenotti <malenottinicco@gmail.com>
2025-07-07 15:59:01 +0200

    Fix build errors and update GUI

--- cmd/meshspy/main.go
+++ cmd/meshspy/main.go
@@ -28,7 +28,6 @@ import (
 const (
 	welcomeMessage = "Ciao da MeshSpy, presto (spero) per tutti"
 	aliveMessage   = "MeshSpy Alive"
-
 )
 
 // Version of the MeshSpy program. This value can be overridden at build time
@@ -70,10 +69,14 @@ func main() {
 	}
 
 	if *msg != "" {
-		if err := mqtt.SendText(cfg.SerialPort, *dest, *msg); err != nil {
+		if err := serial.SendText(cfg.SerialPort, *msg); err != nil {
 			log.Fatalf("❌ Errore invio messaggio: %v", err)
 		}
-		log.Printf("✅ Messaggio inviato a %s", *dest)
+		if *dest != "" {
+			log.Printf("✅ Messaggio inviato a %s", *dest)
+		} else {
+			log.Printf("✅ Messaggio inviato")
+		}
 		return
 	}
 
